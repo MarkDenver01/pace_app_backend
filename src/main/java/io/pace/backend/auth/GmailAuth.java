2c3030fa665a1071a6a6bb3a23354b8df6caed09
Denver <gregoriomarkdenver01@gmail.com>
2025-10-16 10:53:48 +0800

    update gauth local

---
+++ src/main/java/io/pace/backend/auth/GmailAuth.java
@@ -0,0 +1,38 @@
+package io.pace.backend.auth;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.GmailScopes;
+
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class GmailAuth {
+    private static final List<String> SCOPES = List.of(GmailScopes.GMAIL_SEND,
+            GmailScopes.GMAIL_READONLY);
+
+    public static void main(String[] args) throws Exception {
+        var httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        var jsonFactory = GsonFactory.getDefaultInstance();
+
+        var clientSecrets = GoogleClientSecrets.load(jsonFactory, new FileReader("src/main/resources/client_secret.json"));
+
+        var flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
+
+        var receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+        System.out.println("Tokens stored successfully in: " + Paths.get("tokens").toAbsolutePath());
+    }
+
+}
